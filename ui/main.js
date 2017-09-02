78f80e07fddd846fc912ae6d9c8cd529feb19eca
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-02 15:04:56 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = "new value";
+
+var img = document.getElementById('madi');
+img.onclick = function () {
+  img.style.marginLeft = '100px';  
+};
+
