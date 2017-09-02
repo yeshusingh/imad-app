2bd7ad48b23965672c1be86e15f7ae06b5a38bc6
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-02 12:21:31 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,43 @@ var articleOne = {
                 </p>`
 };
 
+func createTemplate(data) {
+    
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+        </div>
+    </body>
+    </html>
+    `;
+}
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
