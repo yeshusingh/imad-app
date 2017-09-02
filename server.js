bb49b420563cf69759e6eb55bc4a49c04e7d7004
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-02 13:11:54 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var articleOne = {
                 </p>`
 };
 
-function createTemplate(data) {
+function createTemplate (data) {
     
     var title = data.title;
     var heading = data.heading;
