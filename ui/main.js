2875b37e18bffb7570f01f904816e80cea25938e
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-02 15:11:07 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,14 @@ var element = document.getElementById('main-text');
 element.innerHTML = "new value";
 
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img,style.marginLeft = marginLeft + 'px';
+}
+
 img.onclick = function () {
-  img.style.marginLeft = '100px';  
+  //img.style.marginLeft = '100px';  
+  var interval = setInterval(moveRight, 100);
 };
 
