9b225a779b527fbc34cc22e41b4e491c75b04d5c
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-02 15:12:45 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,6 @@ function moveRight() {
 
 img.onclick = function () {
   //img.style.marginLeft = '100px';  
-  var interval = setInterval(moveRight, 100);
+  var interval = setInterval(moveRight, 50);
 };
 
