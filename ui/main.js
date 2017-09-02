a5456f21a8a23f0d0edeeb1c4c85ced19e42f1ef
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-02 15:11:33 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 10;
-    img,style.marginLeft = marginLeft + 'px';
+    img.style.marginLeft = marginLeft + 'px';
 }
 
 img.onclick = function () {
