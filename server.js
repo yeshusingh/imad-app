f1ce166fc7b24d8b71197b6537aab04c66012e4d
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-02 10:42:05 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function(req, res){
-    res.send('Article one is being served here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 })
 
 app.get('/article-two', function(req, res){
