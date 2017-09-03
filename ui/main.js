8e5e1394208beeb2a6aa26d5488948c5429cc871
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-03 10:09:18 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,60 +1,60 @@
 console.log('Loaded!');
 
 
-var button = document.getElementById('counter');
-//var counter = 0;
-button.onclick = function () {
-    var request = new XMLHttpRequest();
+// var button = document.getElementById('counter');
+// //var counter = 0;
+// button.onclick = function () {
+//     var request = new XMLHttpRequest();
     
-    request.onreadystatechange = function () {
-        //console.log(`${request.status}`);
+//     request.onreadystatechange = function () {
+//         //console.log(`${request.status}`);
         
-        if(request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
+//         if(request.readyState === XMLHttpRequest.DONE) {
+//             if (request.status === 200) {
+//                 var counter = request.responseText;
+//                 var span = document.getElementById('count');
+//                 span.innerHTML = counter.toString();
+//             }
+//         }
+//     };
     
-    // counter =  counter + 1;
-    // var span = document.getElementById('count');
-    // span.innerHTML = counter.toString();
+//     // counter =  counter + 1;
+//     // var span = document.getElementById('count');
+//     // span.innerHTML = counter.toString();
     
-    request.open('GET', 'http://yeshusingh.imad.hasura-app.io/counter', true);
-    request.send(null);
+//     request.open('GET', 'http://yeshusingh.imad.hasura-app.io/counter', true);
+//     request.send(null);
 
-};
+// };
 
 
-var submit = document.getElementById('submit_btn');
-submit.onclick = function() {
+// var submit = document.getElementById('submit_btn');
+// submit.onclick = function() {
   
-  var request = new XMLHttpRequest();
+//   var request = new XMLHttpRequest();
     
-    request.onreadystatechange = function () {
-        //console.log(`${request.status}`);
+//     request.onreadystatechange = function () {
+//         //console.log(`${request.status}`);
         
-        if(request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                  var names = request.responseText;
-                  names = JSON.parse(names);
-                  var list = '';
-                  for(var i=0; i<names.length; i++) {
-                      list += '<li>' + names[i] + '</li>';
-                  }
-                  var ul = document.getElementById('namelist');
-                  ul.innerHTML = list; 
-            }
-        }
-    };
+//         if(request.readyState === XMLHttpRequest.DONE) {
+//             if (request.status === 200) {
+//                   var names = request.responseText;
+//                   names = JSON.parse(names);
+//                   var list = '';
+//                   for(var i=0; i<names.length; i++) {
+//                       list += '<li>' + names[i] + '</li>';
+//                   }
+//                   var ul = document.getElementById('namelist');
+//                   ul.innerHTML = list; 
+//             }
+//         }
+//     };
     
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET', 'http://yeshusingh.imad.hasura-app.io/submit-name?name='+ name, true);
-    request.send(null);
-};
+//     var nameInput = document.getElementById('name');
+//     var name = nameInput.value;
+//     request.open('GET', 'http://yeshusingh.imad.hasura-app.io/submit-name?name='+ name, true);
+//     request.send(null);
+// };
 
 var submitComment = document.getElementById('submit_comment');
 submitComment.onclick = function() {
