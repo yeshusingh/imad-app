204a6dcea53aec45f3b867282774ea193d9ef76e
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-03 10:05:24 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -56,27 +56,27 @@ submit.onclick = function() {
     request.send(null);
 };
 
-// var submitComment = document.getElementById('submit_comment');
-// submitComment.onclick = function() {
+var submitComment = document.getElementById('submit_comment');
+submitComment.onclick = function() {
   
-//   var request = new XMLHttpRequest();
+  var request = new XMLHttpRequest();
     
-//     request.onreadystatechange = function () {
-//         //console.log(`${request.status}`);
+    request.onreadystatechange = function () {
+        //console.log(`${request.status}`);
         
-//         if(request.readyState === XMLHttpRequest.DONE) {
-//             if (request.status === 200) {
-//                   var comments = request.responseText;
-//                   comments = JSON.parse(comments);
-//                   var list = '';
-//                   for(var i=0; i<comments.length; i++) {
-//                       list += '<li>' + comments[i] + '</li>';
-//                   }
-//                   var ul = document.getElementById('commentlist');
-//                   ul.innerHTML = list; 
-//             }
-//         }
-//     };
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                  var comments = request.responseText;
+                  comments = JSON.parse(comments);
+                  var list = '';
+                  for(var i=0; i<comments.length; i++) {
+                      list += '<li>' + comments[i] + '</li>';
+                  }
+                  var ul = document.getElementById('commentlist');
+                  ul.innerHTML = list; 
+            }
+        }
+    };
     
 //     var commentInput = document.getElementById('comment');
 //     var comment = commentInput.value;
