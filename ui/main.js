64946f871cad3472998e3609ea12163a2a81286d
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-02 21:06:04 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,26 +2,26 @@ console.log('Loaded!');
 
 
 var button = document.getElementById('counter');
-//var counter = 0;
+var counter = 0;
 button.onclick = function () {
     console.log('button clicked');
     var request = new XMLHttpRequest();
     
-    request.onreadystatechange = function () {
-        console.log('test1');
-        if(request.ReadyState === XMLHttpRequest.DONE) {
-            if (request.state === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-                 span.innerHTML = 99;
-            }
-        }
-    };
+    // request.onreadystatechange = function () {
+    //     console.log('test1');
+    //     if(request.ReadyState === XMLHttpRequest.DONE) {
+    //         if (request.state === 200) {
+    //             var counter = request.responseText;
+    //             var span = document.getElementById('count');
+    //             span.innerHTML = counter.toString();
+    //              span.innerHTML = 99;
+    //         }
+    //     }
+    // };
     
-    //counter =  counter + 1;
-    //var span = document.getElementById('count');
-    //span.innerHTML = counter.toString();
+    counter =  counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
     
     request.open('GET', 'http://yeshusingh.imad.hasura-app.io/counter', true);
     request.send(null);
