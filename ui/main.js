4ea9d29bff02f79e3af02d8337becabc8afd122d
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-02 17:22:22 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,18 @@
 console.log('Loaded!');
 
 
-var element = document.getElementById('main-text');
-element.innerHTML = "new value";
+// var element = document.getElementById('main-text');
+// element.innerHTML = "new value";
 
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
+// var img = document.getElementById('madi');
+// var marginLeft = 0;
+// function moveRight() {
+//     marginLeft = marginLeft + 5;
+//     img.style.marginLeft = marginLeft + 'px';
+// }
 
-img.onclick = function () {
-  //img.style.marginLeft = '100px';  
-  var interval = setInterval(moveRight, 50);
-};
+// img.onclick = function () {
+//   //img.style.marginLeft = '100px';  
+//   var interval = setInterval(moveRight, 50);
+// };
 
