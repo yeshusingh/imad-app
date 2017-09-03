bdcce1cfe3fcf7c27a1254ffad4289ea955c8438
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-02 23:40:28 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,6 +32,24 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
   
+  var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function () {
+        console.log(`${request.status}`);
+        
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    request.open('GET', 'http://yeshusingh.imad.hasura-app.io/counter', true);
+    request.send(null);
+  
+  
   var names = ['name1', 'name2', 'name3', 'name4'];
   var list = '';
   for(var i=0; i<names.length; i++) {
