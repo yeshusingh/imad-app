1d439641a694c4d3acaf9c2e32724d2e63309090
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-02 23:42:27 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,7 +41,7 @@ submit.onclick = function() {
             if (request.status === 200) {
                   var names = ['name1', 'name2', 'name3', 'name4'];
                   var list = '';
-                  for(var i= 0; i<names.length; i++) {
+                  for(var i=0; i<names.length; i++) {
                       list += '<li>' + names[i] + '</li>';
                   }
                   var ul = document.getElementById('namelist');
