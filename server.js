831aae6f62c86036d36f982f7cd30348490b2b08
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-02 17:31:00 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+
 var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
