d5c09befb24ef48492914403b65f93933283f7eb
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-02 23:53:49 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,8 +27,7 @@ button.onclick = function () {
 
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
   
@@ -51,6 +50,8 @@ submit.onclick = function() {
         }
     };
     
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://yeshusingh.imad.hasura-app.io/submit-name?name=', name, true);
     request.send(null);
   
