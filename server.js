8f36241ea372796b4dea24012bae18d71ed8f5f6
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-10 15:36:50 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user: 'yeshusingh',
@@ -16,6 +17,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use( session({
+    secret: 'someRandomSecretValue',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 *30}
+}));
 
 var articles = {
     'article-one' : {
