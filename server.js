4d597aaadb444613aa1424a12cc29506406425dc
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-09 21:59:55 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -143,10 +143,24 @@ app.get('/article-one1', function(req, res) {
     res.send(JSON.stringify(comments));
 }) ;
 
-app.get('/:articleName', function(req, res) {
+app.get('/articles/:articleName', function(req, res) {
     //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
     var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    
+    pool.query("select * from article where title = " + req.params.articleName, function(err, result) {
+       if (err) {
+           res.status(500).send(err.toString());
+       } else {
+           if(result.rows.length === 0) {
+               res.status(404).send('Article not found');
+           } else {
+               var articleData = result.rows[0];
+               res.send(createTemplate(articleData));
+           }
+       }
+    });
+    
+    
 });
 
 // app.get('/article-two', function(req, res){
