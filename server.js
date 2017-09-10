f28042c4a834aeeff98a773d1602fa6c92f458ed
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-10 11:33:08 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ app.post('/login', function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
     
-    pool.query('select * from "yuser" username = $1', [username], function (err, result) {
+    pool.query('select * from "yuser" where username = $1', [username], function (err, result) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
