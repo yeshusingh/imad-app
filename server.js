2f3b1bef23dfc8ea0fa657bdf13d7bfcb75728de
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-10 09:35:15 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,12 +97,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-function hash(input) {
-    var hashed = ctypto.pbkdf2Sync();   
+function hash(input, salt) {
+    var hashed = ctypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
 }
 
 app.get('/hash/:input', function(req, res) {
-   var hashedString = hash(req.params.input);
+   var hashedString = hash(req.params.input, 'This-is-some-random-string');
    res.send(hashedString);
 });
 
