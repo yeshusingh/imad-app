f06ec27d2a5502ac095c33bb26b87da9f6ebfb9a
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-10 11:01:43 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/', function (req, res) {
 });
 
 function hash(input, salt) {
-    var hashed = ctypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     //return hashed.toString('hex');
     return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
 }
