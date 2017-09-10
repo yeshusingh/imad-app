1bfe3e45c77143d6c4289c59ad471ea0028c40c9
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-09 21:27:06 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'yeshusingh',
+    database: 'yeshusingh',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
