83e9da8747d7fe3fc3c8847c221d0bd108f3916a
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-10 11:28:21 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,12 +131,23 @@ app.post('/login', function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
     
-
     pool.query('select * from "yuser" username = $1', [username], function (err, result) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
-            res.send('User successfully created: ' + username);
+            if (result.rows.length === 0) {
+                res.send(403).send('username/password is invalid');
+            } else {
+                var dbString = result.rows[0].password;
+                var salt = dbString.split('$')[2];
+                var hashedPassword = hash(password, salt);
+                if (hashedPassword === dbString) {
+                    res.send('credentials correct!');
+                } else {
+                    res.send(403).send('username/password is invalid');
+                }
+            }
+            
         }
     });
 });
