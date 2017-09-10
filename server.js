a6c519af613d3cc447c7fbc8bf1cc00d52418e35
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-10 16:30:43 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -169,6 +169,7 @@ app.get('/check-login', function(req, res) {
 
 app.get('/logout', function(req, res) {
   delete req.session.auth;
+  res.send('logged out');
 });
 
 var pool = new Pool(config);
