85f818a18b52a567667993fb45b0a29201b3f01c
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-10 10:51:28 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var ctypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'yeshusingh',
@@ -14,6 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var articles = {
     'article-one' : {
