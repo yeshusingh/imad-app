f748c4804e3c365f30b3e53e362d362394af2277
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-10 11:00:46 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-var ctypto = require('crypto');
+var crypto = require('crypto');
 var bodyParser = require('body-parser');
 
 var config = {
