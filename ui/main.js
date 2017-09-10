c26bfebbd9f860031df02a845eff580306b6784a
yeshusingh <yeshusingh@users.noreply.github.com>
2017-09-10 15:09:59 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -63,7 +63,6 @@ submit2.onclick = function() {
     
     request.onreadystatechange = function () {
         //console.log(`${request.status}`);
-        
         if(request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
                   alert('Logged in successfully');
@@ -81,6 +80,7 @@ submit2.onclick = function() {
     console.log(password);
     
     request.open('POST', 'http://yeshusingh.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));
 };
 
